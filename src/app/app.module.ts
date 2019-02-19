3c747df7e560d55ef23790fec3893c7055b3f62e
Hovhannes Arzumanyan <hovhannesa@energizeglobal.com>
2019-02-19 15:14:41 +0400

    17. Using Pipes to Transform Output

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { ShortenPipe } from './shorten.pipe';
+import { FilterPipe } from './filter.pipe';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ShortenPipe,
+    FilterPipe
   ],
   imports: [
     BrowserModule,
