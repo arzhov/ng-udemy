9b51b29698ce5b7bb0bba19db762e338632856f5
Hovhannes Arzumanyan <hovhannesa@energizeglobal.com>
2019-02-25 15:53:31 +0400

    20. Authentication & Route Protection in Angular Apps

--- src/app/shared/data-storage.service.ts
+++ src/app/shared/data-storage.service.ts
@@ -3,17 +3,25 @@ import { HttpClient } from '@angular/common/http';
 
 import { RecipeService } from '../recipes/recipe.service';
 import { Recipe } from '../recipes/recipe.model';
+import { AuthService } from '../auth/auth.service';
 
 @Injectable()
 export class DataStorageService {
-  constructor(private http: HttpClient, private recipeService: RecipeService) {}
+  constructor(private http: HttpClient,
+              private recipeService: RecipeService,
+              private authService: AuthService) {
+  }
 
   storeRecipes() {
-    return this.http.put('https://ng-test-c719d.firebaseio.com/recipes.json', this.recipeService.getRecipes());
+    const token = this.authService.getToken();
+
+    return this.http.put('https://ng-test-c719d.firebaseio.com/recipes.json?auth=' + token, this.recipeService.getRecipes());
   }
 
   getRecipes() {
-    this.http.get<Recipe[]>('https://ng-test-c719d.firebaseio.com//recipes.json')
+    const token = this.authService.getToken();
+
+    this.http.get('https://ng-test-c719d.firebaseio.com/recipes.json?auth=' + token)
       .subscribe(
         (recipes: Recipe[]) => {
           for (const recipe of recipes) {
